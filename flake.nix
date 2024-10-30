48d6999fddff0c48ec5651c09deee4e3191de73c
Ryan Faulhaber <ryf@sent.as>
2024-10-30 11:43:18 -0400

    project init

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "fdate-rs";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    projectName = "fdate-rs";
+    supportedSystems = ["x86_64-linux"];
+    forSystems = systems: f:
+      nixpkgs.lib.genAttrs systems
+      (system: f system (import nixpkgs {inherit system;}));
+    forAllSystems = forSystems supportedSystems;
+  in {
+    packages = forAllSystems (system: pkgs: {
+      packages.${projectName} = pkgs.rustPlatform.buildRustPackage {
+        pname = projectName;
+        version = "0.1.0";
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+      };
+      default = self.packages.${system}.projectName;
+    });
+
+    formatter = forAllSystems (system: pkgs: pkgs.alejandra);
+
+    devShells = forAllSystems (system: pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cargo
+          rustc
+          rustfmt
+          clippy
+          rust-analyzer
+          rustup
+        ];
+      };
+    });
+  };
+}
